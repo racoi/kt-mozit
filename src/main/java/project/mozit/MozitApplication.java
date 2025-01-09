00366775073862b537a8dbbed4a622b2c6dc7cdc
yeonju <919jung@gmail.com>
2025-01-09 15:52:19 +0900

    init

---
+++ src/main/java/project/mozit/MozitApplication.java
@@ -0,0 +1,14 @@
+package project.mozit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class MozitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MozitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/mozit/MozitApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mozit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
