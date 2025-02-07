cf28a1b530e6fe7e3d62c256615338fd9acea9d4
yeonju <919jung@gmail.com>
2025-02-07 17:50:05 +0900

    azure metrics

--- src/main/java/project/mozit/controller/AzureMetricsController.java
+++ src/main/java/project/mozit/controller/AzureMetricsController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -29,14 +31,18 @@ public class AzureMetricsController {
 
     @GetMapping("/metrics")
     public ResponseEntity<?> getAzureMetrics() {
-        // 고정된 URL
+        System.out.println("SAS Token: " + sasToken);
+
         String storageUrl = "https://mozitstorage.blob.core.windows.net/insights-metrics-pt1m/resourceId=/SUBSCRIPTIONS/0A938E62-00BA-4C73-A908-3B285014B302/RESOURCEGROUPS/MOZIT/PROVIDERS/MICROSOFT.DBFORMYSQL/FLEXIBLESERVERS/MOZIT-DB/y=2025/m=02/d=05/h=06/m=00/PT1H.json";
 
         try {
-            // 요청 보내기
             ResponseEntity<String> response = restTemplate.exchange(storageUrl + "?" + sasToken, HttpMethod.GET, null, String.class);
-            return ResponseEntity.ok(response.getBody());  // 응답 그대로 반환
+            return ResponseEntity.ok(response.getBody());
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            // HTTP 오류 처리
+            return ResponseEntity.status(e.getStatusCode()).body("Error fetching metrics: " + e.getMessage());
         } catch (Exception e) {
+            // 다른 예외 처리
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching metrics: " + e.getMessage());
         }
     }
